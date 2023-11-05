458ac986e1322d40f53529d2e51c3bcf8cbc0a53
Hasnaa Ahmed <123015937+hasnaa02ahmed@users.noreply.github.com>
2023-11-05 23:34:07 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
-#include <iostream>
 
+//Names                     ID           Emails
+//Shahd Mohammed Mahmoud  20221086     moh861368@gmail.com
+//Hasnaa Ahmed Eltahlawy   20221232    hasnaa02ahmed@gmail.com
+//Menna Mahmoud Abass      20221234    mennaalmoazamy10@gmail.com
+//section 20,19
+#include <iostream>
+#include "BigReal.h"
+
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    BigReal n1 ("11.9000000000000000000000000000000001");
+    BigReal n2 ("2333333333339.1134322222222292");
+    BigReal n3 = n1 + n2;
+    cout << n3<<endl;
+    BigReal n4("0.9");
+    n3=n3+n4;
+    cout << n3;
     return 0;
 }
